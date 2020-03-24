fc2da4830f5cc9102c2c79312ff3aea3c79a9f1f
agmo <mor.agnieszka@gmail.com>
2020-03-24 21:05:56 +0100

    Update Angular to v9

--- src/app/error-handler/custom-error-handler.ts
+++ src/app/error-handler/custom-error-handler.ts
@@ -1,14 +1,20 @@
-import {ErrorHandler, Injectable} from '@angular/core';
+import {ErrorHandler, Injectable, Injector} from '@angular/core';
 import {ErrorLoggerService} from './error-logger.service';
 import {ToastrService} from 'ngx-toastr';
 
 @Injectable()
 export class CustomErrorHandler implements ErrorHandler {
+  private toastrService: ToastrService;
 
-  constructor(private errorLoggerService: ErrorLoggerService, private toastrService: ToastrService) {
+  constructor(private errorLoggerService: ErrorLoggerService, private injector: Injector) {
   }
 
   handleError(error: any): void {
+    // Injecting the ToastrService into the constructor results in a cyclic dependency error. Using the injector as a workaround.
+    if (!this.toastrService) {
+      this.toastrService = this.injector.get(ToastrService);
+    }
+
     console.error(error);
     this.toastrService.error(
       'An error was intercepted by the custom error handler. See the dev console for error details.',
