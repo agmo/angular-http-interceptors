68e80324954ef8f42ebdf57ee4ebe9121ea2a0c2
agmo <mor.agnieszka@gmail.com>
2020-04-06 22:09:50 +0200

    Rename the Auth Interceptor file for consistency

--- src/app/http-interceptors/index.ts
+++ src/app/http-interceptors/index.ts
@@ -2,7 +2,7 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import {ErrorInterceptor} from './error.interceptor';
-import {AuthInterceptor} from './auth-interceptor';
+import {AuthInterceptor} from './auth.interceptor';
 
 
 /** Http interceptor providers in outside-in order */
