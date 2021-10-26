f23bcc6746aad31fc9936454c94f063841e05458
agmo <mor.agnieszka@gmail.com>
2021-10-26 19:57:43 +0200

    Add a simple caching interceptor

---
+++ src/app/http-interceptors/caching.interceptor.ts
@@ -0,0 +1,31 @@
+import {Injectable} from '@angular/core';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse} from '@angular/common/http';
+import {Observable, of} from 'rxjs';
+import {tap} from 'rxjs/operators';
+
+@Injectable()
+export class CachingInterceptor implements HttpInterceptor {
+  private cache = new Map<string, any>(); // cache could be stored in indexedDB
+
+  constructor() {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    if (request.method !== 'GET') {
+      return next.handle(request);
+    }
+
+    const cachedResponse = this.cache.get(request.url);
+
+    if (cachedResponse) {
+      return of(cachedResponse);
+    }
+
+    return next.handle(request).pipe(
+      tap(event => {
+        if (event instanceof HttpResponse) {
+          this.cache.set(request.url, event);
+        }
+      })
+    );
+  }
+}
